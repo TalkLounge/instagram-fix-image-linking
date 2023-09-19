db7eaa856fb61cb81147f8515948695e1f229daa
TalkLounge <talklounge@yahoo.de>
2023-09-19 23:07:50 +0200

    Add Reel Support & Fix Video Mute

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Instagram: Fix Image Linking
 // @name:de         Instagram: Bildlink beheben
-// @version         1.1.0
+// @version         1.1.1
 // @description     Fix Right Click on Image/Video to Download & User Mentions Button on Instagram browsing as Guest
 // @description:de  Behebe Rechtsklick aufs Bild/Video zum Herunterladen & User Verlinkungen/Erwähnungen Button auf Instagram beim Browsen als Gast
 // @icon            https://static.cdninstagram.com/rsrc.php/y4/r/QaBlI0OZiks.ico
@@ -9,20 +9,21 @@
 // @namespace       https://github.com/TalkLounge/instagram-fix-image-linking
 // @license         MIT
 // @match           https://www.instagram.com/p/*
+// @match           https://www.instagram.com/reel/*
 // @grant           none
 // ==/UserScript==
 
 (function () {
     'use strict';
 
-	function init() {
-		const imgs = [...document.querySelectorAll("article img[style]")].concat([...document.querySelectorAll("article video")]);
+    function init() {
+        const imgs = [...document.querySelectorAll("article img[style]")].concat([...document.querySelectorAll("article video")]);
 
-		if (! imgs.length) {
-			return;
-		}
+        if (!imgs.length) {
+            return;
+        }
 
-		clearInterval(interval);
+        clearInterval(interval);
 
         let actives = [];
         for (let i = 0; i < imgs.length; i++) {
@@ -32,18 +33,25 @@
             // Cursor
             imgs[i].parentNode.parentNode.parentNode.style.cursor = "default";
 
+            // Fix video mute
+            if (imgs[i].tagName == "VIDEO") {
+                imgs[i].addEventListener("play", (event) => {
+                    event.target.muted = false;
+                });
+            }
+
             // Fix user mentions
             actives[i] = false;
 
-            if (! imgs[i].parentNode.parentNode.parentNode.querySelector("button svg")) { // Skip when no user mentions
+            if (!imgs[i].parentNode.parentNode.parentNode.querySelector("button svg")) { // Skip when no user mentions
                 continue;
             }
 
-            imgs[i].parentNode.parentNode.parentNode.querySelector("button svg").parentNode.parentNode.onclick = function() {
+            imgs[i].parentNode.parentNode.parentNode.querySelector("button svg").parentNode.parentNode.onclick = function () {
                 const j = i;
                 event.stopPropagation();
 
-                if (! actives[j]) {
+                if (!actives[j]) {
                     actives[j] = true;
 
                     imgs[j].parentNode.parentNode.parentNode.querySelectorAll("a").forEach((item) => {
@@ -60,7 +68,7 @@
                 }
             }
         }
-	}
+    }
 
-	const interval = window.setInterval(init, 500);
+    const interval = window.setInterval(init, 500);
 })();
