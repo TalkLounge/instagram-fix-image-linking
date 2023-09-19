5c6cb35b12f336b6a0ca5efe4bcf2ece00c3fc02
TalkLounge <talklounge@yahoo.de>
2023-09-19 20:04:27 +0200

    Add Carousel and Video Support & Prevent Login Popup on User Mentions Click & Add Userscript Icon

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name            Instagram: Fix Image Linking
 // @name:de         Instagram: Bildlink beheben
-// @version         1.0.0
-// @description     Fix Right Click on Image to Download & User Mentions Button on Instagram browsing as Guest
-// @description:de  Behebe Rechtsklick aufs Bild zum Herunterladen & User Verlinkungen/Erwähnungen Button auf Instagram beim Browsen als Gast
+// @version         1.1.0
+// @description     Fix Right Click on Image/Video to Download & User Mentions Button on Instagram browsing as Guest
+// @description:de  Behebe Rechtsklick aufs Bild/Video zum Herunterladen & User Verlinkungen/Erwähnungen Button auf Instagram beim Browsen als Gast
+// @icon            https://static.cdninstagram.com/rsrc.php/y4/r/QaBlI0OZiks.ico
 // @author          TalkLounge (https://github.com/TalkLounge)
 // @namespace       https://github.com/TalkLounge/instagram-fix-image-linking
 // @license         MIT
@@ -13,42 +14,53 @@
 
 (function () {
     'use strict';
-	
-	function init() {		
-		const img = document.querySelector("img[crossorigin]");
-		
-		if (! img) {
+
+	function init() {
+		const imgs = [...document.querySelectorAll("article img[style]")].concat([...document.querySelectorAll("article video")]);
+
+		if (! imgs.length) {
 			return;
 		}
-		
-		clearInterval(i);
 
-		// Right click to download image
-		img.parentNode.parentNode.querySelector("div:nth-child(2)").remove();
+		clearInterval(interval);
 
-		// Cursor
-		img.parentNode.parentNode.parentNode.style.cursor = "default";
-		
-		// Fix user mentions
-		let active = false;
-		document.querySelector("button svg").parentNode.parentNode.onclick = function() {
-			if (! active) {
-				active = true;
+        let actives = [];
+        for (let i = 0; i < imgs.length; i++) {
+            // Right click to download image
+            imgs[i].parentNode.parentNode.querySelector("div:nth-child(2)").remove();
 
-				img.parentNode.parentNode.parentNode.querySelectorAll("a").forEach((o) => {
-					o.parentNode.style.opacity = 1;
-					o.parentNode.style.transform = "translate(-50%, 0%)";
-					o.parentNode.style.pointerEvents = "inherit"
-				});
-			} else {
-				active = false;
+            // Cursor
+            imgs[i].parentNode.parentNode.parentNode.style.cursor = "default";
 
-				img.parentNode.parentNode.parentNode.querySelectorAll("a").forEach((o) => {
-					o.parentNode.style.transform = "translate(-50%, 0%) scale(0)";
-				});
-			}
-		};
+            // Fix user mentions
+            actives[i] = false;
+
+            if (! imgs[i].parentNode.parentNode.parentNode.querySelector("button svg")) { // Skip when no user mentions
+                continue;
+            }
+
+            imgs[i].parentNode.parentNode.parentNode.querySelector("button svg").parentNode.parentNode.onclick = function() {
+                const j = i;
+                event.stopPropagation();
+
+                if (! actives[j]) {
+                    actives[j] = true;
+
+                    imgs[j].parentNode.parentNode.parentNode.querySelectorAll("a").forEach((item) => {
+                        item.parentNode.style.opacity = 1;
+                        item.parentNode.style.transform = item.parentNode.style.transform.replace(" scale(0)", "");
+                        item.parentNode.style.pointerEvents = "inherit"
+                    });
+                } else {
+                    actives[j] = false;
+
+                    imgs[j].parentNode.parentNode.parentNode.querySelectorAll("a").forEach((item) => {
+                        item.parentNode.style.opacity = 0;
+                    });
+                }
+            }
+        }
 	}
-	
-	const i = window.setInterval(init, 500);
+
+	const interval = window.setInterval(init, 500);
 })();
